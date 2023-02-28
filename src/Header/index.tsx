259580ab111ba10dc37072ee1c7155bbef73353d
Denis <den.kochkin36@mail.ru>
2023-03-01 00:27:59 +0300

    Initialized the Pc-Enjoyer

---
+++ src/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav>
+      <div className="container-navbar">
+        <ul className="navbar-menu">
+          <li><a>ОБНОВЛЕНИЯ</a></li>
+          <li><a>КОМПЛЕКТУЮЩИЕ</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
