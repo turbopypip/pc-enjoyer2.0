ab555bba012cf864a04e8f0b10afc87d2679b606
Denis <den.kochkin36@mail.ru>
2023-07-01 15:26:20 +0300

    Added identification circle to selected page and fixed common issues with phone version

--- src/Navigation/index.tsx
+++ src/Navigation/index.tsx
@@ -32,6 +32,9 @@ const Navigation: FC<Props> = ({ setPage, showNavigation, isMobile, page }) => {
         <li className="Pc-EnjoyerLogo">
           <a onClick={(e) => handleClick(e, 0)}>PC-Enjoyer</a>
         </li>
+
+        <hr className="dividingLine"/>
+
         <li className="PageLinks">
           {page === 1
             ?
@@ -41,27 +44,70 @@ const Navigation: FC<Props> = ({ setPage, showNavigation, isMobile, page }) => {
           <a onClick={(e) => handleClick(e, 1)}>Материнская плата</a>
         </li>
         <li className="PageLinks">
+          {page === 2
+            ?
+            <SelectedPageIdentificator />
+            :
+            <></>}
           <a onClick={(e) => handleClick(e, 2)}>Центральный процессор</a>
         </li>
         <li className="PageLinks">
+          {page === 3
+            ?
+            <SelectedPageIdentificator />
+            :
+            <></>}
           <a onClick={(e) => handleClick(e, 3)}>Блок питания</a>
         </li>
         <li className="PageLinks">
+          {page === 4
+            ?
+            <SelectedPageIdentificator />
+            :
+            <></>}
           <a onClick={(e) => handleClick(e, 4)}>Оперативная память</a>
         </li>
         <li className="PageLinks">
+          {page === 5
+            ?
+            <SelectedPageIdentificator />
+            :
+            <></>}
           <a onClick={(e) => handleClick(e, 5)}>Видеокарта</a>
         </li>
         <li className="PageLinks">
+          {page === 6
+            ?
+            <SelectedPageIdentificator />
+            :
+            <></>}
           <a onClick={(e) => handleClick(e, 6)}>Корпус</a>
         </li>
         <li className="PageLinks">
+          {page === 7
+            ?
+            <SelectedPageIdentificator />
+            :
+            <></>}
           <a onClick={(e) => handleClick(e, 7)}>Система охлаждения</a>
         </li>
         <li className="PageLinks">
+          {page === 8
+            ?
+            <SelectedPageIdentificator />
+            :
+            <></>}
           <a onClick={(e) => handleClick(e, 8)}>Накопители памяти</a>
         </li>
+
+        <hr className="dividingLine"/>
+
         <li className="Updates">
+          {page === 9
+            ?
+            <SelectedPageIdentificator />
+            :
+            <></>}
           <a onClick={(e) => handleClick(e, 9)}>Обновления</a>
         </li>
       </ul>
